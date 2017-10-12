11484403f8d33cbf378e799ff0c6fa02be65b9d1
zhihui.xu <zhihui.xu@qunar.com>
2017-10-12 14:29:27 +0800

    first commit

---
+++ src/main/scala/zhihui/xu/spark/App.scala
@@ -0,0 +1,9 @@
+package zhihui.xu.spark
+
+/**
+ * Hello world!
+ *
+ */
+object App extends Application {
+  println( "Hello World!" )
+}
---
+++ src/test/scala/zhihui/xu/spark/AppTest.scala
@@ -0,0 +1,17 @@
+package zhihui.xu.spark
+
+import org.junit._
+import Assert._
+
+@Test
+class AppTest {
+
+    @Test
+    def testOK() = assertTrue(true)
+
+//    @Test
+//    def testKO() = assertTrue(false)
+
+}
+
+
